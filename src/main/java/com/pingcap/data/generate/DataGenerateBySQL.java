2b67143b73e15e7add6c7f5890b50eab9f52f8ad
lizhenhuan <1916038084@qq.com>
2022-09-26 00:18:55 +0800

    add DataGenerateBySQL

--- src/main/java/com/pingcap/data/generate/DataGenerateBySQL.java
+++ src/main/java/com/pingcap/data/generate/DataGenerateBySQL.java
@@ -5,7 +5,6 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -14,16 +13,16 @@ import java.util.concurrent.atomic.AtomicLong;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class DataGenerate {
-    private static final Logger LOGGER = LoggerFactory.getLogger(DataGenerate.class);
+public class DataGenerateBySQL {
+    private static final Logger LOGGER = LoggerFactory.getLogger(DataGenerateBySQL.class);
     private static int THREAD_NUM;
     private static Long TOTAL_SIZE;
     private static String IP_PORT;
     private static String USER;
     private static String PASSWORD;
     private static String DB_NAME;
-    private static String TABLE_NAME;
-    private static int BATCH_SIZE = 500;
+    private static String SQL;
+    private static int ONCE_INSERT_COUNT;
     private static AtomicLong atomicLong = new AtomicLong();
 
     public static void main(String[] args) {
@@ -41,8 +40,7 @@ public class DataGenerate {
                 Connection connection = DriverManager.getConnection(url, USER, PASSWORD);
                 connectionList.add(connection);
             }
-            List<ColumnInfo> columnInfoList = MetaUtil.getMeta(connectionList.get(0), DB_NAME, TABLE_NAME);
-            String insertSQL = MetaUtil.generateInsertSQL(columnInfoList, DB_NAME, TABLE_NAME);
+            String insertSQL = SQL;
             LOGGER.info("insert SQL:" + insertSQL);
 
             Long start = System.currentTimeMillis();
@@ -50,7 +48,7 @@ public class DataGenerate {
                 executorService.execute(
                         () -> {
                             try {
-                                insertData(connection, insertSQL, columnInfoList);
+                                insertData(connection, insertSQL);
                             } catch (Exception e) {
                                 LOGGER.error("send message error", e);
                             } finally {
@@ -72,37 +70,28 @@ public class DataGenerate {
             LOGGER.error("error happen here,", e);
         }
     }
-    private static void insertData(Connection connection, String sql, List<ColumnInfo> columnInfoList) throws Exception {
-        Random random = new Random();
+    private static void insertData(Connection connection, String sql) throws Exception {
         try (PreparedStatement ps =  connection.prepareStatement(sql);) {
-            for (int count = 0; count < TOTAL_SIZE / THREAD_NUM / BATCH_SIZE; count ++) {
-                for (int i = 0; i < BATCH_SIZE; i++) {
-                    for (int j = 0; j < columnInfoList.size(); j++) {
-                        columnInfoList.get(j).setPreparedStatement(ps, j + 1, random);
-                    }
-                    ps.addBatch();
-                }
-                int[] countArr = ps.executeBatch();
-                atomicLong.addAndGet(countArr.length);
-                LOGGER.info(String.format("insert into %s.%s count %s， total count: %s", DB_NAME, TABLE_NAME, countArr.length, atomicLong.get()));
+            for (int count = 0; count < TOTAL_SIZE / THREAD_NUM / ONCE_INSERT_COUNT; count ++) {
+                ps.execute();
+                atomicLong.addAndGet(ONCE_INSERT_COUNT);
+                LOGGER.info(String.format("execute input SQL, once insert count: %s， total count: %s", ONCE_INSERT_COUNT, atomicLong.get()));
             }
         }
     }
 
     private static void parseArgs(String [] args) {
-        if (args.length == 7 || args.length == 8) {
+        if (args.length == 8) {
             THREAD_NUM = Integer.parseInt(args[0]);
             TOTAL_SIZE = Long.parseLong(args[1]);
             IP_PORT = args[2];
             USER = args[3];
             PASSWORD = args[4];
             DB_NAME = args[5];
-            TABLE_NAME = args[6];
-            if (args[7] != null && args[7].length() > 0) {
-                BATCH_SIZE = Integer.parseInt(args[7]);
-            }
+            SQL = args[6];
+            ONCE_INSERT_COUNT = Integer.parseInt(args[7]);
         } else {
-            LOGGER.error("please input 7 args: THREAD_NUM TOTAL_SIZE  IP_PORT USER PASSWORD  DB_NAME TABLE_NAME (BATCH_SIZE) ");
+            LOGGER.error("please input 7 args: THREAD_NUM TOTAL_SIZE  IP_PORT USER PASSWORD  SQL ONCE_INSERT_COUNT ");
             System.exit(1);
         }
     }
