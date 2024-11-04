b1e4ae031f0af8f2ac68c9022497ace126c15f70
lizhenhuan <1916038084@qq.com>
2024-11-04 21:06:12 +0800

    add test add batch

---
+++ src/main/java/com/pingcap/data/generate/TestAddBatch.java
@@ -0,0 +1,98 @@
+package com.pingcap.data.generate;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.util.Date;
+
+public class TestAddBatch {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(TestTiDB.class);
+    private static String IP_PORT;
+    private static String USER;
+    private static String PASSWORD;
+    private static String DB;
+    private static Connection connection;
+
+    public static void main(String[] args) {
+        parseArgs(args);
+        String driver = "com.mysql.jdbc.Driver";
+        String url = String.format("jdbc:mysql://%s/%s?&zeroDateTimeBehavior=convertToNull&useSSL=false&&useConfigs=maxPerformance&rewriteBatchedStatements=true&allowMultiQueries=true&useServerPrepStmts=true&prepStmtCacheSqlLimit=2147483640&cachePrepStmts=true", IP_PORT, DB);
+
+        try {
+            Class.forName(driver);
+            connection = DriverManager.getConnection(url, USER, PASSWORD);
+
+
+            executeBatchReplaceSQL(100);
+            executeBatchReplaceSQL(100);
+            executeBatchReplaceSQL(100);
+            executeBatchReplaceSQL(100);
+
+
+            //executeReplaceSQL();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    private static void executeBatchInsertSQL() throws Exception {
+        Long startTime = new Date().getTime();
+        String sql = "insert into t values(?,?)";
+        LOGGER.info("execute sql : " + sql );
+        PreparedStatement preparedStatementCreate = connection.prepareStatement(sql);
+        for (int i = 0; i < 100; i++) {
+            preparedStatementCreate.setInt(1,i);
+            preparedStatementCreate.setString(2,i + "str");
+            preparedStatementCreate.addBatch();
+        }
+        preparedStatementCreate.executeBatch();
+        Long endTime = new Date().getTime();
+        LOGGER.info("execute sql end cost time : " + (endTime - startTime)  );
+    }
+
+    private static void executeReplaceSQL() throws Exception {
+        Long startTime = new Date().getTime();
+        String sql = "replace into t values(?,?)";
+        LOGGER.info("execute sql : " + sql);
+        try (PreparedStatement preparedStatementCreate = connection.prepareStatement(sql);) {
+            for (int i = 100; i < 101; i++) {
+                preparedStatementCreate.setInt(1, i);
+                preparedStatementCreate.setString(2, i + "str");
+                preparedStatementCreate.execute();
+            }
+        }
+        Long endTime = new Date().getTime();
+        LOGGER.info("execute sql end cost time : " + (endTime - startTime) );
+    }
+
+    private static void executeBatchReplaceSQL(int count) throws Exception {
+        Long startTime = new Date().getTime();
+        String sql = "replace into t values(?,?)";
+        LOGGER.info("execute sql : " + sql);
+        PreparedStatement preparedStatementCreate = connection.prepareStatement(sql);
+        for (int i = 0; i < count; i++) {
+            preparedStatementCreate.setInt(1,i);
+            preparedStatementCreate.setString(2,i + "str");
+            preparedStatementCreate.addBatch();
+        }
+        preparedStatementCreate.executeBatch();
+        Long endTime = new Date().getTime();
+        LOGGER.info("execute sql end cost time : " + (endTime - startTime) );
+    }
+
+    private static void parseArgs(String [] args) {
+        if (args.length == 4) {
+            IP_PORT = args[0];
+            USER = args[1];
+            PASSWORD = args[2];
+            DB = args[3];
+        } else {
+            LOGGER.error("please input 5 args: IP_PORT USER PASSWORD DB SQL");
+            System.exit(1);
+        }
+    }
+}
